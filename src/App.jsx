35b7fea2842ac0e0ab22177c95c2b1ef83d425a4
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-29 15:06:21 +0100

    Updated App.jsx in src

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css'
+
+import Container from "./components/Container"
+import Head from "./components/Head"
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <Container/>
+    </div>
+  )
+}
+
+export default App
