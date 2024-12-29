ea503cdc32c5dd7ad9ce5b153fdd7c33be835cf8
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-29 15:06:25 +0100

    Updated Container.jsx in src/components

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import Counter from "./Counter";
+import Squares from "./Squares";
+
+export default function Container() {
+  return(
+    <div className="container">
+      <Counter />
+      <Squares />
+    </div>
+      )
+};
