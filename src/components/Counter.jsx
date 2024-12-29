d21b721daf7d8387e41b258024ff61d8e87931d5
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-29 15:06:26 +0100

    Updated Counter.jsx in src/components

---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+export default function Counter() {
+  return (
+    <div className="relative">
+      <div className="absulute">
+        <h1>60</h1>
+        <div className="time">seconds</div>
+      </div>
+    </div>
+  )
+};
