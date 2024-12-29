0044167e24b75f78e159b83105a35836d028dfc8
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-29 15:06:30 +0100

    Updated Squares.jsx in src/components

---
+++ src/components/Squares.jsx
@@ -0,0 +1,11 @@
+import Square from "./Square";
+
+export default function Squares() {
+  return (
+    <div className="squares-container">
+      <Square title="words/min" value="0"/>
+      <Square title="words/min" value="0"/>
+      <Square title="words/min" value="0"/>
+    </div>
+  )
+};
